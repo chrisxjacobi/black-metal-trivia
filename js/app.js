e9adb32ab038a5cdef285aa06727beec44cbbb47
chrisxjacobi <chrisxjacobi@gmail.com>
2017-12-01 17:00:50 -0600

    testing object functionality, wrote pseudocode and tested a simple 'start game' function, will later implement

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 var questions = {
+    question: "who is the best?",
     choices: ["you", "me", "them"],
     good: "hello",
 };
@@ -8,18 +9,21 @@ $(document).ready(function () {
     $(".image-area").hide();
 
     $("#start-button").click(function () {
-        $(".hero-text-box").hide("slow", function () {
-            $(".image-area").show("slow", function () {
-                $(this).html("<img id='PBJ' src='http://assets.teamrock.com/image/423100e0-1c69-4917-91e4-e9d44fca2aba?w=800' />");
-            });
-            $(".question-area").show("slow", function () {
+        $(".hero-text-box").fadeOut("slow", function () {
+            $(".question-area").fadeIn("slow", function () {
+                $(this).append(questions.question);
+                $(this).append("<br>");
                 $(this).append(questions.choices);
                 $(this).append("<br>");
-                $(this).next().append(questions.good);
+                $(this).append(questions.good);
+            });
+            $(".image-area").fadeIn("slow", function () {
+                $(this).html("<img id='PBJ' src='http://assets.teamrock.com/image/423100e0-1c69-4917-91e4-e9d44fca2aba?w=800' />");
             });
         });
+
     });
-
-
-
 });
+
+// put in function for starting game
+// put in function for submit/finishing game
