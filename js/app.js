fe0ba4475fcb55cbecd84c1f062f06109f52cc8c
chrisxjacobi <chrisxjacobi@gmail.com>
2017-12-01 14:28:58 -0600

    beginning jquery function with animations, testing initial text appearing to screen

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
-document.querySelector('#start-button').addEventListener('click', function() {
-   document.getElementById('start-button').textContent = " "; 
+var questions = {
+    choices: ["you", "me", "them"],
+    good: "hello",
+};
+
+$(document).ready(function () {
+    $(".question-area").hide();
+
+    $("#start-button").click(function () {
+        $(".hero-text-box").hide("slow", function () {
+            $(".question-area").show("slow", function () {
+                $(this).append(questions.choices);
+                $(this).append("<br>");
+                $(this).append(questions.good);
+            });
+        });
+    });
+
+
+
 });
