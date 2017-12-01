f58310dc0a25d663a6e604d40525f2f1d34d3140
chrisxjacobi <chrisxjacobi@gmail.com>
2017-12-01 15:06:13 -0600

    having a question area and an image appear on start

--- js/app.js
+++ js/app.js
@@ -5,13 +5,17 @@ var questions = {
 
 $(document).ready(function () {
     $(".question-area").hide();
+    $(".image-area").hide();
 
     $("#start-button").click(function () {
         $(".hero-text-box").hide("slow", function () {
+            $(".image-area").show("slow", function () {
+                $(this).html("<img id='PBJ' src='http://assets.teamrock.com/image/423100e0-1c69-4917-91e4-e9d44fca2aba?w=800' />");
+            });
             $(".question-area").show("slow", function () {
                 $(this).append(questions.choices);
                 $(this).append("<br>");
-                $(this).append(questions.good);
+                $(this).next().append(questions.good);
             });
         });
     });
